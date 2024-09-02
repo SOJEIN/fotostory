846bccd5564f895a4666a4131be13ea63f6aa001
Jeisson Ochoa <jeisson.ochoa21@gmail.com>
2024-09-02 09:29:09 -0500

    1.0.6

--- src/components/Gallery/hooks/useUploadFiles.jsx
+++ src/components/Gallery/hooks/useUploadFiles.jsx
@@ -1,9 +1,10 @@
-import { uploadBytesResumable, getDownloadURL, ref } from "firebase/storage";
+import { uploadBytes, getDownloadURL, ref } from "firebase/storage";
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import { storage, db } from "../../../service/firebaseConfig";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
+import { v4 as uuidv4 } from "uuid";
 
 const useUploadFiles = () => {
   const queryClient = useQueryClient();
@@ -25,44 +26,27 @@ const useUploadFiles = () => {
 
   const mutation = useMutation({
     mutationFn: async ({ event }) => {
-      console.log("Mutation function started");
-      console.log("Storage:", storage);
-      console.log("Ref function:", ref);
-
       const file = event.target.files[0];
       if (!file) {
-        throw new Error("No file selected");
+        throw new Error("Seleccione un archivo");
       }
-
-      console.log("File selected:", file);
-
       try {
-        console.log("Storage object:", storage);
-
-        const storageRef = ref(storage, `images/${file.name}`);
-        console.log("Storage reference created:", storageRef);
-
-        const uploadTask = uploadBytesResumable(storageRef, file);
-        console.log("uploadTask", uploadTask);
-        const urlFile = await getDownloadURL(uploadTask.snapshot.ref);
-
-        console.log("File uploaded successfully, URL:", urlFile);
-
+        const ui = uuidv4();
+        const storageRef = ref(storage, `images/${ui + "_" + file.name}`);
+        const uploadTask = await uploadBytes(storageRef, file);
+        const urlFile = await getDownloadURL(storageRef);
         await addDoc(collection(db, "images"), {
           url: urlFile,
           createdAt: new Date(),
         });
-
         setImages((prevImages) => [...prevImages, urlFile]);
         toast.success("Imagen subida y URL guardada en Firestore");
       } catch (error) {
-        console.error("Error uploading file:", error);
         toast.error("Error al subir la imagen");
         throw error;
       }
     },
     onError: (error) => {
-      console.error("Upload failed:", error);
       toast.error("Error durante la subida de la imagen.");
     },
   });
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { assets: "/src/assets" } },
-  base: "/fotostory/",
+  //base: "/fotostory/",
 });
